d913858c9a5d5dcf8efa5949b0606fb041ff98a2
nikhilnitnaware49 <nikhilnitnaware99@gmail.com>
2023-02-28 11:12:09 +0530

    my fierst code

--- src/fixeddeposit/homeloan.java
+++ src/fixeddeposit/homeloan.java
@@ -3,8 +3,8 @@ package fixeddeposit;
 public class homeloan {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("apply for home loan");
 	}
 
 }
